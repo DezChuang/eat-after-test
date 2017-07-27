e3945fd1826a9e5c90dce121e962d2eea3657d9b
dezchuang <dissaivent@gmail.com>
2017-07-27 20:22:36 +0800

    [02][All][Add] Question 2 Init with Add/Delete function

---
+++ 02_CRUD/README.md
@@ -0,0 +1,11 @@
+# 02 CRUD
+
+#### Usage
+
+```
+npm install
+npm run server
+npm start
+```
+
+#### Refernece
---
+++ 02_CRUD/src/components/posts_new.js
@@ -0,0 +1,87 @@
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { createPost } from "../actions";
+
+class PostsNew extends Component {
+  renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? "has-danger" : ""}`;
+
+    return (
+      <div className={className}>
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+        <div className="text-help">
+          {touched ? error : ""}
+        </div>
+      </div>
+    );
+  }
+
+  onSubmit(values) {
+    this.props.createPost(values, () => {
+      this.props.history.push("/");
+    });
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          label="Title"
+          name="title"
+          component={this.renderField}
+        />
+        <Field
+          label="Author"
+          name="author"
+          component={this.renderField}
+        />
+        <Field
+          label="Year Published"
+          name="year_published"
+          component={this.renderField}
+        />
+        <Field
+          label="Rating"
+          name="rating"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
+      </form>
+    );
+  }
+}
+
+function validate(values) {
+  // console.log(values) -> { title: 'asdf', categories: 'asdf', content: 'asdf' }
+  const errors = {};
+
+  // Validate the inputs from 'values'
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+  if (!values.author) {
+    errors.author = "Enter an author";
+  }
+  if (!values.year_published) {
+    errors.year_published = "Enter year this book published";
+  }
+  if (!values.rating) {
+    errors.rating = "Enter rating about this book";
+  }
+
+  // If errors is empty, the form is fine to submit
+  // If errors has *any* properties, redux form assumes form is invalid
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: "PostsNewForm"
+})(connect(null, { createPost })(PostsNew));
